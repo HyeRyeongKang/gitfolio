14012d1ccfb90d37c6f708c4312cd7e4acd6bb3e
boyunjjang1 <boyunqueen@gmail.com>
2018-08-24 21:58:26 +0900

    Modify mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age,:phone,:job,:address,:github,:nick,:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:age,:phone,:job,:address,:github,:nick,:avatar])
    end
   
 end
